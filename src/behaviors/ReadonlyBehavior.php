79570210755c594bbcb98353d86fc8bbcdc93c33
Smoren <smoren@PC103619.PKRVMN.local>
2021-06-09 12:44:30 +0300

    readonly behavior

---
+++ src/behaviors/ReadonlyBehavior.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Smoren\Yii2\ActiveRecordExplicit\behaviors;
+
+use Smoren\Yii2\ActiveRecordExplicit\exceptions\DbException;
+use Smoren\Yii2\ActiveRecordExplicit\models\ActiveRecord;
+use yii\base\Behavior;
+use yii\base\Event;
+use yii\db\BaseActiveRecord;
+
+/**
+ * Поведение позволяет задать аттрибуты AR модели только для чтения.
+ */
+class ReadonlyBehavior extends Behavior
+{
+    /**
+     * @var array Список аттрибутов
+     */
+    public $attributes;
+
+    /**
+     * Список
+     * @return array
+     */
+    public function events()
+    {
+        return [
+            BaseActiveRecord::EVENT_BEFORE_INSERT => 'checkIt',
+            BaseActiveRecord::EVENT_BEFORE_UPDATE => 'checkIt',
+        ];
+    }
+
+    /**
+     * Проверяем, что значения изменились.
+     * @param Event $event
+     * @throws DbException
+     */
+    public function checkIt(Event $event)
+    {
+        /** @var ActiveRecord $model */
+        $model = $this->owner;
+
+        foreach($this->attributes as $attribute) {
+            if($model->getAttribute($attribute) != $model->getOldAttribute($attribute)) {
+                throw new DbException("you cannot change {$attribute} attribute", DbException::STATUS_LOGIC_ERROR);
+            }
+        }
+    }
+}
