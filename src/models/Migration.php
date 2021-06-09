dc5c93e4cc5bab3bfa22b3900d09bd81572b60d7
Smoren <smoren@PC103619.PKRVMN.local>
2021-06-09 11:14:30 +0300

    upd

---
+++ src/models/Migration.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Smoren\Yii2\Microcore\base;
+
+use yii\base\NotSupportedException;
+use yii\db\ColumnSchemaBuilder;
+
+/**
+ * Class Migration
+ * @package Smoren\Yii2\Microcore\base
+ */
+class Migration extends \yii\db\Migration
+{
+    /**
+     * @return ColumnSchemaBuilder
+     * @throws NotSupportedException
+     */
+    public function uuidPrimaryKey()
+    {
+        return $this->getDb()->getSchema()->createColumnSchemaBuilder('uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4()');
+    }
+
+    /**
+     * @return ColumnSchemaBuilder
+     * @throws NotSupportedException
+     */
+    public function uuid()
+    {
+        return $this->getDb()->getSchema()->createColumnSchemaBuilder('uuid');
+    }
+}
