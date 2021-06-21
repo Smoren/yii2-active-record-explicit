c5f4cb407af9f19268ff4060afb7d9b41f51ee14
Smoren <ofigate@gmail.com>
2021-06-21 15:30:47 +0300

    migration auto generated uuid

--- src/models/Migration.php
+++ src/models/Migration.php
@@ -24,6 +24,15 @@ class Migration extends \yii\db\Migration
      * @return ColumnSchemaBuilder
      * @throws NotSupportedException
      */
+    public function uuidAutoGen()
+    {
+        return $this->getDb()->getSchema()->createColumnSchemaBuilder('uuid NOT NULL DEFAULT uuid_generate_v4()');
+    }
+
+    /**
+     * @return ColumnSchemaBuilder
+     * @throws NotSupportedException
+     */
     public function uuid()
     {
         return $this->getDb()->getSchema()->createColumnSchemaBuilder('uuid');
